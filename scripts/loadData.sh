1f27e2d181a2deff14ddd4c4caf7a627b5e98d2d
Mi Zhang <jasmineustc@gmail.com>
2015-12-01 17:22:37 -0500

    Update loadData.sh

--- scripts/loadData.sh
+++ scripts/loadData.sh
@@ -12,6 +12,10 @@ sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "
       LINES TERMINATED BY '\n';"
         echo "$f finish" 
 done
+
+sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "CREATE INDEX useridIndex ON test2 (userid,newDate);"
+echo create index q2 finish
+
 echo finishQ2
 #######################################
 # load q3 data
@@ -26,7 +30,12 @@ sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "
       LINES TERMINATED BY '\n';"
         echo "$f finish" 
 done
+
+sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "CREATE INDEX useridIndex ON test3 (userid);"
+echo create index q3 finish 
+
 echo finishQ3
+
 #######################################
 # load q4 data
 #######################################
@@ -40,6 +49,9 @@ sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "
       LINES TERMINATED BY '\n';"
         echo "$f finish" 
 done
+
+sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "CREATE INDEX tagIndex ON test4 (hashTag);"
+echo create index q4 finish
 
 echo finishQ4
 #######################################
@@ -55,6 +67,10 @@ sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "
       LINES TERMINATED BY '\n';"
         echo "$f finish" 
 done
+
+sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "CREATE INDEX idIndex ON test5 (userid,nextid);"
+echo create index q5 finish
+
 echo finishQ5
 
 
@@ -71,5 +87,8 @@ sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "
       LINES TERMINATED BY '\n';"
         echo "$f finish" 
 done
+
+sudo mysql --local-infile -uroot -p123456 mydb -e "use mydb" -e "CREATE INDEX tweetidIndex ON test6 (tweetid);"
+echo create index q6 finish
 echo finishQ6
 
